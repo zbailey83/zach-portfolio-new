f35d7606126e48176b7ca7bad2c1eff497c24806
zer0cool <zbailey83@users.noreply.github.com>
2023-09-08 15:57:12 -0600

    Add files via upload

--- pages/about/index.js
+++ pages/about/index.js
@@ -1,44 +1,24 @@
+import React, {useState} from 'react';
+
 // icons
 import {
-  FaHtml5,
+  FaHTML5,
   FaCss3,
   FaJs,
   FaReact,
-  FaWordpress,
-  FaFigma,
-} from "react-icons/fa";
+  FaWordPress,
+  FaPython,
+} from 'react-icons/fa';
 
 import {
   SiNextdotjs,
   SiFramer,
   SiAdobexd,
   SiAdobephotoshop,
-} from "react-icons/si";
+} from 'react-icons/si';
 
-
-//  data
-const aboutData = [
-  {
-    title: 'skills',
-    info: [
-      {
-        title: 'Web Development',
-        icons: [
-          <FaHtml5 />,
-          <FaCss3 />,
-          <FaJs />,
-          <FaReact />,
-          <SiNextdotjs />,
-          <SiFramer />,
-          <FaWordpress />,
-        ],
-      },
-      {
-        title: 'UI/UX Design',
-        icons: [<FaFigma />, <SiAdobexd />, <SiAdobephotoshop />],
-      },
-    ],
-  },
+// about data
+export const aboutData = [
   {
     title: 'awards',
     info: [
@@ -88,8 +68,34 @@ const aboutData = [
   },
 ];
 
+// components
+import Avatar from '../../components/Avatar';
+import Circles from '../../components/Circles';
+
+import { motion } from 'framer-motion';
+import { fadeIn } from '../../variants';
+
 const About = () => {
-  return <div>About</div>;
+  const [index, setIndex] = useState(0);
+  return (
+  <div className='h-full bg-primary/30 py-32 text-center xl:text-left'>
+    <Circles />
+    {/* avatar img */}
+    <motion.div 
+      variants={fadeIn('right', 0.2)}
+      initial='hidden'
+      animate='show'
+      exit='hidden' 
+      className='hidden xl:flex absolute bottom-0 -left-[370px]'
+      >
+      <Avatar />
+    </motion.div>
+    <div className='container mx-auto h-full flex flex-col items-center xl:flex-row gap-x-6'>
+      <div>text</div>
+      <div>about</div>
+    </div>
+  </div>
+  );
 };
 
 export default About;
